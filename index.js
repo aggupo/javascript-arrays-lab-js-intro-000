d57a97db66331628b04728b5fc8a041669445388
Li Ge <gelber.libi@gmail.com>
2018-04-08 15:12:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const app = "I don't do much."
 
-let kittens = ['Milo', 'Otis', 'Garfield'];
+const kittens = ['Milo', 'Otis', 'Garfield'];
 
 function destructivelyAppendKitten(name) {
   return kittens.push(name);

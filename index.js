1cdbee707cc3b59d6d355c8178677b1cfc8d6647
Li Ge <gelber.libi@gmail.com>
2018-04-08 14:58:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
 const app = "I don't do much."
 
 var kittens = ['Milo', 'Otis', 'Garfield'];
+
+// describe('destructivelyAppendKitten(name)', function() {
+//   it('appends a kitten to the end of the kittens array', function() {
+//     destructivelyAppendKitten('Ralph')
+//     expect(window.kittens).toEqual(["Milo", "Otis", "Garfield", "Ralph"])
+// 
+//     
+fanction destructivelyAppendKitten(name) {
+  kittent.push(name);
+  return kittent;
+}

1a42c5926d83ba4258e971f2cddcb533f8b165f9
Li Ge <gelber.libi@gmail.com>
2018-04-08 15:03:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,3 +9,8 @@ function destructivelyAppendKitten(name) {
 function destructivelyPrependKitten(name) {
   return kittens.unshift(name);
 }
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop();
+  return kittens;
+}
